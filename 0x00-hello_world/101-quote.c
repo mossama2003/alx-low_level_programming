b064ce1d07151b910507b35471656791b1764d9e
mossama2003 <mossama370@gmail.com>
2023-06-15 19:54:46 -0700

    First c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,18 @@
-#include <>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a qoute using write function
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fputs(text, stderr);
+	return (1);
+}
+
