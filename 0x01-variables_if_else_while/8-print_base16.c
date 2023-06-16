00d2857e44b5409a8acc01f533eb7ff572beeb68
mossama2003 <mossama370@gmail.com>
2023-06-16 16:22:20 -0700

    First c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,13 +10,12 @@
 
 int main(void)
 {
-	int digit = 48; /*48: decimal rep of 0*/
+	int digit = 48;
 
-	while (digit <= 120) /*120: decimal rep of f*/
+	while (digit <= 102)
 	{
 		putchar(digit);
 
-		/* after 9 we jump till 96; */
 		if (digit == 57)
 			digit += 39;
 		digit++;
