b18f958663d46ad55fd42458899403125e6a829f
mossama2003 <mossama370@gmail.com>
2023-06-16 19:20:00 -0700

    First c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -26,9 +26,9 @@ int main(void)
 						digit2 != digit3 &&
 						digit2 < digit3)
 				{
-					putchar('digit1 + 48');
-					putchar('digit2 + 48');
-					putchar('digit3 + 48');
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
 
 					if (digit1 + digit2 + digit3 != 24)
 					{
