55a6ceecaf0c0e17057fef431a2645572fdfdf9e
mossama2003 <mossama370@gmail.com>
2023-06-20 16:15:16 -0700

    first c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (mult = 1; mult <= 9; num++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
