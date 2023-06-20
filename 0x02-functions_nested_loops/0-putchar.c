9d457ba83e0cf6cea06a3be543b9336557a12c2b
mossama2003 <mossama370@gmail.com>
2023-06-20 08:00:24 -0700

    First c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Enrgy point
+ *
+ * Description: program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
