3bab5f00f391cff491a8eafc159d4994bf6a8b6f
mossama2003 <mossama370@gmail.com>
2023-06-20 09:23:58 -0700

    first c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * a function that prints the alphabet, in lowercase,
+*/
+
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
