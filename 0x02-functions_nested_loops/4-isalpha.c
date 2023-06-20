b0020f0d21958ac048c826bee8ace5b0d2ee1e53
mossama2003 <mossama370@gmail.com>
2023-06-20 13:13:14 -0700

    first c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,11 +11,11 @@
 
 int _isalpha(int c)
 {
-	for (C >= 97 && C <= 122)
+	for (C >= 97 && c <= 122)
 	{
 		return (1);
 	}
-	for (C >= 65 && C <= 90)
+	for (C >= 65 && c <= 90)
 	{
 		return (1);
 	}
