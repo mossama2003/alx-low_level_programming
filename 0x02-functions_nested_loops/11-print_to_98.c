03dbccb1d79fd2d3a2efd0b611a5121244bf6973
mossama2003 <mossama370@gmail.com>
2023-06-20 16:45:27 -0700

    first c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,13 +19,13 @@ void print_to_98(int n)
 		{
 			printf("%d. ", count);
 		}
-		else
-		{
-			for (count = n; count < 98; count++)
-			{
-				printf("%d. ", count);
-			}
-		}
-		printf("98\n");
 	}
+	else
+	{
+		for (count = n; count < 98; count++)
+		{
+			printf("%d. ", count);
+		}
+	}
+	printf("98\n");
 }
