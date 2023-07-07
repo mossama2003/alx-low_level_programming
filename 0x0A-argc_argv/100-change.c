1c1392a38da05e417976406a0c00479b9cd3aec7
mossama2003 <mossama370@gmail.com>
2023-07-07 15:41:47 -0700

    first c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,39 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
- * main - prints the minimun number of coins
- * to take change for an amount of money.
- * @argc: number of commad line arguments.
- * @argv: pointer to an array of command line arguments.
- * Return: 0 - success, non - zero - fail.
-*/
+ * main - entry point
+ * @argc: character
+ * @argv: character
+ *
+ * Description: -
+ * Return: -
+ */
 
 int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-		int i, leastcents = 0, money = atoi(argv[1]);
-		int cents[] = {25, 10, 5, 2, 1};
+	int coin;
+	int total = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int i = 0;
 
-		for (i = 0; i < 5; i++)
-		{
-			if (money >= cents[i])
-			{
-				leastcents += money / cents[i];
-				money = money % cents[i];
-				if (money % cents[i] == 0)
-				{
-					break;
-				}
-			}
-		}
-		pritnf("%d\n", leastcents);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
 	}
-        else
-        {
-                printf("Error\n");
-                return (1);
-        }
+	for (i = 1; i < (int)strlen(argv[1]); i++)
+	{
+		if (!isdigit(argv[1][i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	coin = atoi(argv[1]);
+	while (coin > 0)
+	{
+		i = 0;
+		while (i < 5)
+		{
+			if (coins[i] <= coin)
+			{
+				coin -= coins[i];
+				total++;
+				break;
+			}
+			i++;
+		}
+	}
+	printf("%d\n", total);
 	return (0);
 }
