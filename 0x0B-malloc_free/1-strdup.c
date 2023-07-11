81741173b1e3993a022d3b0ec326f91e54544fba
mossama2003 <mossama370@gmail.com>
2023-07-10 17:18:19 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,6 +11,7 @@ char *_strdup(char *str)
 {
 	int i = 0, size = 0;
 	char *m;
+
 	if (str == NULL)
 	{
 		return (NULL);
@@ -19,7 +20,7 @@ char *_strdup(char *str)
 	{
 		;
 	}
-	m = malloc(size * sizeof(*str) +1);
+	m = malloc(size * sizeof(*str) + 1);
 	if (m == 0)
 	{
 		return (NULL);
