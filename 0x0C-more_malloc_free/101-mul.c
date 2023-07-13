3d120fadfff93bb3c6bca6c3103451b12af0f25b
mossama2003 <mossama370@gmail.com>
2023-07-13 14:51:07 -0700

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -30,7 +30,7 @@ int _atoi(const char *s)
 
 	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
-		if (s[firstNum] == '-')
+		if (s[firstNum] == '_')
 		{
 			sign *= -1;
 		}
