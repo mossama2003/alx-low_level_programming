a4307a12c2ff221d4aad66aa1ca380049e5a04bb
mossama2003 <mossama370@gmail.com>
2023-07-13 15:57:13 -0700

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
- * is_digit - check if a string contains a non-digit char
+ * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
-*/
-
+ */
 int is_digit(char *s)
 {
 	int i = 0;
@@ -16,9 +17,7 @@ int is_digit(char *s)
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
-		{
 			return (0);
-		}
 		i++;
 	}
 	return (1);
@@ -29,8 +28,7 @@ int is_digit(char *s)
  * @s: string to evaluate
  *
  * Return: the length of the string
-*/
-
+ */
 int _strlen(char *s)
 {
 	int i = 0;
@@ -43,13 +41,12 @@ int _strlen(char *s)
 }
 
 /**
- * errors - handles error  for main
-*/
-
-void error(void)
+ * errors - handles errors for main
+ */
+void errors(void)
 {
 	printf("Error\n");
-	exit (98);
+	exit(98);
 }
 
 /**
@@ -58,8 +55,7 @@ void error(void)
  * @argv: array of arguments
  *
  * Return: always 0 (Success)
-*/
-
+ */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
@@ -67,21 +63,15 @@ int main(int argc, char *argv[])
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-	{
-		error();
-	}
+		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
-	{
 		return (1);
-	}
 	for (i = 0; i <= len1 + len2; i++)
-	{
 		result[i] = 0;
-	}
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -94,25 +84,17 @@ int main(int argc, char *argv[])
 			carry /= 10;
 		}
 		if (carry > 0)
-		{
 			result[len1 + len2 + 1] += carry;
-		}
 	}
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
-		{
 			a = 1;
-		}
 		if (a)
-		{
 			_putchar(result[i] + '0');
-		}
 	}
 	if (!a)
-	{
 		_putchar('0');
-	}
 	_putchar('\n');
 	free(result);
 	return (0);
